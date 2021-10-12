4506cdf6ec2cbb0b31ee012135ad577d34091dd7
Jonathan Llewellyn <jonathan.llewellyn27@gmail.com>
2021-10-12 10:14:46 -0600

    added rollbar

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
 
+const rollbar = new Rollbar({
+    accessToken: '5b7049a7f0ca4e0699d217ce4e6eeb65',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+});
+
 const app = express();
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully')
 });
 
 const port = process.env.PORT || 4400
