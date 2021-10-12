83f01db121f82be457a26d30068933051a84d093
Jonathan Llewellyn <jonathan.llewellyn27@gmail.com>
2021-10-12 10:01:09 -0600

    bootstrapped server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+const port = process.env.port || 4400
+
+app.listen(4400, () => {
+    console.log(`Server is running on ${port}!`)
+});
+
